763931b703c727ce379a452475b1dde4aaee6ca3
Janusz Kozerski <j.kozerski@samsung.com>
2014-05-16 12:47:48 +0200

    Rename project to "altimeter"
    Signed-off-by: Janusz Kozerski <janusz.kozerski@gmail.com>

---
+++ src/com/gmail/januszkozerski/altimeter/MainActivity.java
@@ -0,0 +1,208 @@
+package com.gmail.januszkozerski.altimeter;
+
+import java.text.NumberFormat;
+
+import com.gmail.januszkozerski.altimeter.R;
+
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.app.Activity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+    private class MyListener implements SensorEventListener {
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int accuracy)
+        {
+            // Probably not needed
+        }
+
+        @Override
+        public void onSensorChanged(SensorEvent event)
+        {
+            if (counter < max_values) {
+                Log.d("WYS", "counter : " + counter);
+                last_values[counter] = event.values[0];
+                ++counter;
+            } else if (counter == max_values) {
+                Log.d("WYS", "counter : " + counter);
+                double sum = 0;
+                for (int i = 0; i < max_values; ++i) {
+                    sum += last_values[i];
+                }
+                presureHeight.set_presure_start(sum / max_values);
+                Log.d("WYS", "start presure value: " + sum / max_values);
+                textView3.setText(nf2.format(presureHeight.get_presure_start()) + "hPa");
+                ++counter;
+            } else if (counter > max_values) {
+                Log.d("WYS", "presure value: " + event.values[0]);
+                add_avg_value(event.values[0]);
+                presureHeight.set_presure_final(count_agv());
+                // presureHeight.set_presure_final(event.values[0]);
+                textView4.setText(nf2.format(event.values[0]) + "hPa");
+                textView1.setText(nf1.format(presureHeight.get_altitude()) + "m");
+            }
+        }
+    }
+
+    // private SensorManager sensorManager = (SensorManager)
+    // getSystemService(SENSOR_SERVICE);
+    // private Sensor sensor =
+    // sensorManager.getDefaultSensor(Sensor.TYPE_PRESSURE);
+    private SensorManager sensorManager;
+    private Sensor sensor;
+    private MyListener myListenerInstance = new MyListener();
+
+    private static PresureHeight presureHeight = new PresureHeight();
+    private static final int max_values = 15; // How many values to measure to get
+                                              // average pressure (start point)
+    private static double last_values[] = new double[max_values];
+    private static int avg_max = 5;
+    private static double presure_avg[] = new double[avg_max];
+    private static int avg_cnt = 0;
+    private static int counter = 0;
+    private static int is_working = 0;
+
+    // ----
+    private TextView textView1;
+    private TextView textView3;
+    private TextView textView4;
+    // ----
+
+    NumberFormat nf2 = NumberFormat.getNumberInstance();
+    NumberFormat nf1 = NumberFormat.getNumberInstance();
+
+    private static double count_agv()
+    {
+        double sum = 0;
+        for (int i = 0; i < avg_max; ++i) {
+            sum += presure_avg[i];
+        }
+        return sum / avg_max;
+    }
+
+    private static void add_avg_value(double value)
+    {
+        presure_avg[avg_cnt] = value;
+        avg_cnt = (avg_cnt + 1) % avg_max;
+    }
+
+    public static void reset_counter()
+    {
+        counter = 0;
+    }
+
+    private void startMeasurement()
+    {
+        presureHeight.set_temperature_celcius(18.0);
+        // sensorManager.registerListener(myListenerInstance, sensor,
+        // SensorManager.SENSOR_DELAY_NORMAL);
+        sensorManager.registerListener(myListenerInstance, sensor, SensorManager.SENSOR_DELAY_FASTEST);
+        textView3.setText(nf2.format(presureHeight.get_presure_start()) + "hPa");
+        is_working = 1;
+    }
+
+    private void stopMeasurement()
+    {
+        sensorManager.unregisterListener(myListenerInstance);
+        is_working = 0;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        nf2.setMaximumFractionDigits(2);
+        nf2.setMinimumFractionDigits(2);
+
+        nf1.setMaximumFractionDigits(1);
+        nf1.setMinimumFractionDigits(1);
+
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_PRESSURE);
+
+        final Button button1 = (Button) findViewById(R.id.button1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v)
+            {
+                startMeasurement();
+            }
+        });
+
+        final Button button2 = (Button) findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v)
+            {
+                stopMeasurement();
+            }
+        });
+
+        final Button button3 = (Button) findViewById(R.id.button3);
+        button3.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v)
+            {
+                reset_counter();
+            }
+        });
+
+        textView1 = (TextView) findViewById(R.id.textView1);
+        textView3 = (TextView) findViewById(R.id.textView3);
+        textView4 = (TextView) findViewById(R.id.textView4);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    protected void onRestart()
+    {
+        if (is_working == 1) {
+            textView3.setText(nf2.format(presureHeight.get_presure_start()) + "hPa");
+            sensorManager.registerListener(myListenerInstance, sensor, SensorManager.SENSOR_DELAY_FASTEST);
+        } else
+            sensorManager.unregisterListener(myListenerInstance);
+        super.onRestart();
+    }
+
+    protected void onResume()
+    {
+        if (is_working == 1)
+            textView3.setText(nf2.format(presureHeight.get_presure_start()) + "hPa");
+        super.onResume();
+    }
+
+    protected void onPause()
+    {
+        sensorManager.unregisterListener(myListenerInstance);
+        super.onPause();
+    }
+
+    protected void onStop()
+    {
+        sensorManager.unregisterListener(myListenerInstance);
+        super.onStop();
+    }
+
+    protected void onDestroy()
+    {
+        sensorManager.unregisterListener(myListenerInstance);
+        is_working = 0;
+        super.onDestroy();
+    }
+
+}
